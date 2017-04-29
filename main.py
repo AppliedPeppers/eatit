e68b07b6a423bc7ab89e66165ef6b8744cc2d1b4
Vetcher <vetcher@ngs.ru>
2017-04-29 09:07:06 +0300

    qwe

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ import random
 
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
+from flask import *
+
 
 def read_form_exel():
     return pd.read_excel("D:\Files\Work\Prog\eatit\\USDA.xls")
-	
+
+
 s = read_form_exel()
 
 x = s.values[:, 2]
 Y = s.values[:, 1]
+
+voc = {"[1]": "завтрак", "[2]": "обед", "[3]": "ужин", "[4]": "закуска", "[5]": "Ошибка!"}
 
 ingrs_list = []
 for ingrs_str in x:
@@ -21,7 +26,7 @@ for ingrs_str in x:
         for i in ingrs:
             if i not in ingrs_list:
                 ingrs_list.append(i)
-				
+
 table = np.zeros((len(x), len(ingrs_list)), dtype=int)
 
 for ind, ingrs_str in enumerate(x):
@@ -30,17 +35,27 @@ for ind, ingrs_str in enumerate(x):
         for i in ingrs:
             table[ind, ingrs_list.index(i)] = 1
 
-test = SelectKBest(score_func=chi2, k=100)
-fit = test.fit(X, np.array(Y, dtype=int))
+X = table
 
+test = SelectKBest(score_func=chi2, k=50)
+fit = test.fit(X, np.array(Y, dtype=int))
 
 clf = svm.SVC()
 print("LEARNING START")
 clf.fit(fit.transform(X), np.array(Y, dtype=int))
 print("LEARNING END")
 
-a = np.zeros(len(ingrs_list), dtype=int)
-for i, _ in enumerate(a):
-    if random.randint(0, 100) < 30:
-        a[i] = 1
-print(clf.predict(fit.transform(a)))
+app = Flask(__name__)
+
+
+@app.route("/", method=['GET', 'POST'])
+def hello():
+    a = np.zeros(len(ingrs_list), dtype=int)
+    for i, _ in enumerate(a):
+        if random.randint(0, 100) < 15:
+            a[i] = 1
+    return render_template('index.tpl', num=voc[str(clf.predict(fit.transform(a)))])
+
+
+if __name__ == "__main__":
+    app.run()
